393bc27bf8ce6a54f43f9cc10ba869edcc615c95
mehari123 <88460961+mehari123@users.noreply.github.com>
2022-10-11 07:50:47 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
+"""
+Module 0-hbtn-status
+Fetches https://intranet.hbtn.io/status
+"""
+
+
 import urllib.request
-
-
-def fetcher():
-    """fetcher"""
-    req=urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
-
-if __name__ == "__main__":
-    fetcher()
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+print('Body response:')
+print("\t- type: {}".format(type(html)))
+print("\t- content: {}".format(html))
+print("\t- utf8 content: {}".format(html.decode('utf-8')))
