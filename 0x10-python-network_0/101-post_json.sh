d8f310d7fafb0cfbe2071c327a132fe3466dbd9f
mehari123 <88460961+mehari123@users.noreply.github.com>
2022-10-11 09:33:52 +0300

    Add files via upload

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Take in filename and URL, post contents of file; Usage: ./101-post_json.sh 0.0.0.0:5000/route_json my_json_0 ; echo ""
-curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
+# sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -sX POST -H "Content-type: application/json" -H "Accept: application/json" -d "@$2" "$1"
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Take in URL, send DELETE request, display response body; Usage: ./2-delete.sh 0.0.0.0:5000/route_3 ; echo ""
+# script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
 curl -sX DELETE "$1"
